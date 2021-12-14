31456f1e29397e89d02a40d1d9323a1af0abacde
JotaroInukai <92638269+JotaroInukai@users.noreply.github.com>
2021-12-15 01:07:55 +0900

    init

---
+++ basic/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+app_name="basic"
+urlpatterns = [
+    path('templates/', views.basic_template, name='basic_template'),
+    path('templates/basic_form_show', views.basic_form_show, name='basic_form_show'),
+    path('templates/basic_form', views.basic_form, name='basic_form'),
+    path('templates/basic_form_show2', views.basic_form_show2, name='basic_form_show2'),
+    path('templates/basic_form2', views.basic_form2, name='basic_form2'),
+]
