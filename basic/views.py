b66a805601ffaa02154cacd522de3477c9e2145e
JotaroInukai <92638269+JotaroInukai@users.noreply.github.com>
2021-12-23 05:25:42 +0900

    update

--- basic/views.py
+++ basic/views.py
@@ -5,40 +5,10 @@ import random
 import functools
 @functools.lru_cache()
 def func():
-    S1=A1()
-    S2=A2()
-    S3=A3()
-    S4=A4()
-    S5=A5()
-    S6=A6()
-    S7=A7()
-    S8=A8()
-    S9=A9()
-    S10=A10()
-    S21=A21()
-    S22=A22()
-    S23=A23()
-    S24=A24()
-    S25=A25()
-    S11=A11()
-    S12=A12()
-    S13=A13()
-    S14=A14()
-    S15=A15()
-    S16=A16()
-    S17=A17()
-    S18=A18()
-    S19=A19()
-    S20=A20()
-    S26=A26()
-    S27=A27()
-    S28=A28()
-    S29=A29()
-    S30=A30()
-    sagilistA=[S1,S2,S3,S4,S5,S6,S7,S8,S9,S10,S21,S22,S23,S24,S25]
-    hisagilistA=[S11,S12,S13,S14,S15,S16,S17,S18,S19,S20,S26,S27,S28,S29,S30]
-    random.shuffle(sagilistA)
-    random.shuffle(hisagilistA)
+    
+    sagilistA=sagilist()
+    hisagilistA=hisagilist()
+    
     return [sagilistA,hisagilistA]
 # Create your views here.
 
