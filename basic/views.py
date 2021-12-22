e805486d8a8abfc6ec52b648fb4a533c50fc18e0
JotaroInukai <92638269+JotaroInukai@users.noreply.github.com>
2021-12-23 04:07:30 +0900

    update

--- basic/views.py
+++ basic/views.py
@@ -2,40 +2,9 @@ from django.shortcuts import render
 from basic.datas import *
 from basic.defs import *
 import random
-S1=A1()
-S2=A2()
-S3=A3()
-S4=A4()
-S5=A5()
-S6=A6()
-S7=A7()
-S8=A8()
-S9=A9()
-S10=A10()
-S21=A21()
-S22=A22()
-S23=A23()
-S24=A24()
-S25=A25()
-S11=A11()
-S12=A12()
-S13=A13()
-S14=A14()
-S15=A15()
-S16=A16()
-S17=A17()
-S18=A18()
-S19=A19()
-S20=A20()
-S26=A26()
-S27=A27()
-S28=A28()
-S29=A29()
-S30=A30()
-sagilist=[S1,S2,S3,S4,S5,S6,S7,S8,S9,S10,S21,S22,S23,S24,S25]
-hisagilist=[S11,S12,S13,S14,S15,S16,S17,S18,S19,S20,S26,S27,S28,S29,S30]
-random.shuffle(sagilist)
-random.shuffle(hisagilist)
+
+
+
 # Create your views here.
 def basic_template(request):
     return render(request,"basic.html")
@@ -188,3 +157,38 @@ def basic_form4(request):
         }
 
     return render(request,'basicresult4.html',payload)
+
+S1=A1()
+S2=A2()
+S3=A3()
+S4=A4()
+S5=A5()
+S6=A6()
+S7=A7()
+S8=A8()
+S9=A9()
+S10=A10()
+S21=A21()
+S22=A22()
+S23=A23()
+S24=A24()
+S25=A25()
+S11=A11()
+S12=A12()
+S13=A13()
+S14=A14()
+S15=A15()
+S16=A16()
+S17=A17()
+S18=A18()
+S19=A19()
+S20=A20()
+S26=A26()
+S27=A27()
+S28=A28()
+S29=A29()
+S30=A30()
+sagilist=[S1,S2,S3,S4,S5,S6,S7,S8,S9,S10,S21,S22,S23,S24,S25]
+hisagilist=[S11,S12,S13,S14,S15,S16,S17,S18,S19,S20,S26,S27,S28,S29,S30]
+random.shuffle(sagilist)
+random.shuffle(hisagilist)
