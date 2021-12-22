033329a6339522b1be95f5abf2d8e4320f4144da
JotaroInukai <92638269+JotaroInukai@users.noreply.github.com>
2021-12-23 04:15:19 +0900

    update

--- basic/views.py
+++ basic/views.py
@@ -3,7 +3,40 @@ from basic.datas import *
 from basic.defs import *
 import random
 
-
+S1=A1()
+S2=A2()
+S3=A3()
+S4=A4()
+S5=A5()
+S6=A6()
+S7=A7()
+S8=A8()
+S9=A9()
+S10=A10()
+S21=A21()
+S22=A22()
+S23=A23()
+S24=A24()
+S25=A25()
+S11=A11()
+S12=A12()
+S13=A13()
+S14=A14()
+S15=A15()
+S16=A16()
+S17=A17()
+S18=A18()
+S19=A19()
+S20=A20()
+S26=A26()
+S27=A27()
+S28=A28()
+S29=A29()
+S30=A30()
+sagilistA=[S1,S2,S3,S4,S5,S6,S7,S8,S9,S10,S21,S22,S23,S24,S25]
+hisagilistA=[S11,S12,S13,S14,S15,S16,S17,S18,S19,S20,S26,S27,S28,S29,S30]
+random.shuffle(sagilistA)
+random.shuffle(hisagilistA)
 
 # Create your views here.
 def basic_template(request):
@@ -25,16 +58,16 @@ def basic_form_show4(request):
 
 def basic_form(request):
     
-    S1=sagilist[0]
-    S2=sagilist[1]
-    S3=sagilist[2]
-    S4=sagilist[3]
-    S5=sagilist[4]
-    S11=hisagilist[0]
-    S12=hisagilist[1]
-    S13=hisagilist[2]
-    S14=hisagilist[3]
-    S15=hisagilist[4]
+    S1=sagilistA[0]
+    S2=sagilistA[1]
+    S3=sagilistA[2]
+    S4=sagilistA[3]
+    S5=sagilistA[4]
+    S11=hisagilistA[0]
+    S12=hisagilistA[1]
+    S13=hisagilistA[2]
+    S14=hisagilistA[3]
+    S15=hisagilistA[4]
     groupA=[S1,S2,S3,S4,S5,S11,S12,S13,S14,S15]
     random.shuffle(groupA)
 
@@ -70,16 +103,16 @@ def basic_form(request):
 
 
 def basic_form2(request):
-    S1=sagilist[5]
-    S2=sagilist[6]
-    S3=sagilist[7]
-    S4=sagilist[8]
-    S5=sagilist[9]
-    S11=hisagilist[5]
-    S12=hisagilist[6]
-    S13=hisagilist[7]
-    S14=hisagilist[8]
-    S15=hisagilist[9]
+    S1=sagilistA[5]
+    S2=sagilistA[6]
+    S3=sagilistA[7]
+    S4=sagilistA[8]
+    S5=sagilistA[9]
+    S11=hisagilistA[5]
+    S12=hisagilistA[6]
+    S13=hisagilistA[7]
+    S14=hisagilistA[8]
+    S15=hisagilistA[9]
     groupA=[S1,S2,S3,S4,S5,S11,S12,S13,S14,S15]
     random.shuffle(groupA)
 
@@ -115,16 +148,16 @@ def basic_form2(request):
     return render(request,'basicresult2.html',payload)
 
 def basic_form4(request):
-    S1=sagilist[10]
-    S2=sagilist[11]
-    S3=sagilist[12]
-    S4=sagilist[13]
-    S5=sagilist[14]
-    S11=hisagilist[10]
-    S12=hisagilist[11]
-    S13=hisagilist[12]
-    S14=hisagilist[13]
-    S15=hisagilist[14]
+    S1=sagilistA[10]
+    S2=sagilistA[11]
+    S3=sagilistA[12]
+    S4=sagilistA[13]
+    S5=sagilistA[14]
+    S11=hisagilistA[10]
+    S12=hisagilistA[11]
+    S13=hisagilistA[12]
+    S14=hisagilistA[13]
+    S15=hisagilistA[14]
     groupA=[S1,S2,S3,S4,S5,S11,S12,S13,S14,S15]
     random.shuffle(groupA)
 
@@ -158,37 +191,3 @@ def basic_form4(request):
 
     return render(request,'basicresult4.html',payload)
 
-S1=A1()
-S2=A2()
-S3=A3()
-S4=A4()
-S5=A5()
-S6=A6()
-S7=A7()
-S8=A8()
-S9=A9()
-S10=A10()
-S21=A21()
-S22=A22()
-S23=A23()
-S24=A24()
-S25=A25()
-S11=A11()
-S12=A12()
-S13=A13()
-S14=A14()
-S15=A15()
-S16=A16()
-S17=A17()
-S18=A18()
-S19=A19()
-S20=A20()
-S26=A26()
-S27=A27()
-S28=A28()
-S29=A29()
-S30=A30()
-sagilist=[S1,S2,S3,S4,S5,S6,S7,S8,S9,S10,S21,S22,S23,S24,S25]
-hisagilist=[S11,S12,S13,S14,S15,S16,S17,S18,S19,S20,S26,S27,S28,S29,S30]
-random.shuffle(sagilist)
-random.shuffle(hisagilist)
